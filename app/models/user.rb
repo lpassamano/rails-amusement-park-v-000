09c57c9881d6f98a2b5e6e8d338ea482cc8b98b7
lpassamano <leighayna.josele@gmail.com>
2017-11-20 17:22:05 -0500

    added User#mood

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :rides
-  has_many :attractions, through: :rides 
+  has_many :attractions, through: :rides
+
+  def mood
+    if nausea > happiness
+      "sad"
+    elsif nausea < happiness
+      "happy"
+    end
+  end
 end
