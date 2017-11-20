cba948fc68300b09d8b692da9d7b707c66ae8e22
lpassamano <leighayna.josele@gmail.com>
2017-11-20 17:57:50 -0500

    added html to User show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     end
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   private
