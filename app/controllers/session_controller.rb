a164e8ce0cd42fbb9e17134c4ef6e0956eaf9645
lpassamano <leighayna.josele@gmail.com>
2017-11-20 18:20:22 -0500

    added SessionController and loginview

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,13 @@
+class SessionController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#home'
   resources :users, only: [:new, :create, :show]
+
+  get '/signin', to: 'session#new'
+  post 'session', to: 'session#create'
 end
